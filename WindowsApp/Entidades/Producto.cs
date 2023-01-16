d249a56c717cfb32f37b4931d9e54d06528ee0d5
Ivan Benegas <ivanbenegas1996@gmail.com>
2023-01-15 22:18:19 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Entidades/Producto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Entidades
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCosto { get; set; }
+        public double Margen { get; set; }
+        public double IVA { get; set; }
+
+        private decimal _PrecioBruto;
+
+        public decimal PrecioBruto
+        {
+            get
+            {
+                _PrecioBruto = PrecioCosto * Convert.ToDecimal(1 + this.Margen);
+                return _PrecioBruto;
+            }
+        }
+
+        private decimal _PrecioVenta;
+        public decimal PrecioVenta
+        {
+            get
+            {
+                _PrecioVenta = PrecioBruto * Convert.ToDecimal(1 + this.IVA);
+                return _PrecioVenta;
+            }
+        }
+        public string Proveedor { get; set; }
+        public string Categoria { get; set; }
+        public string SubCategoria { get; set; }
+
+
+
+    }
+}
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsApp.Entidades;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearProductoyClienteIndividuo_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto();
+
+            producto.Nombre = "Celular Samsung Z100.";
+            producto.Descripcion = "Último modelo sacado por la empresa Samsung.";
+            producto.PrecioCosto = 1000;
+            producto.Margen = 0.3;
+            producto.IVA = 0.21;
+            producto.Proveedor = "Samsung";
+            producto.Categoria = "Celulares";
+            producto.SubCategoria = "Smartphones";
+
+
+            ClienteIndividuo clienteIndividuo = new ClienteIndividuo();
+
+            clienteIndividuo.Nombre = "Pedro";
+            clienteIndividuo.Apellido = "González";
+            clienteIndividuo.CUIT = "123456789";
+            clienteIndividuo.Email = "pedrogonzalez@gmail.com";
+            clienteIndividuo.Telefono = "987654321";
+            clienteIndividuo.Direccion = "Avenida Hola 123";
+
+            MessageBox.Show(
+                "El nombre del producto es " + producto.Nombre + "\n" +
+                "La descripción es " + producto.Descripcion + "\n" +
+                "El precio de costo es " + producto.PrecioCosto + "\n" +
+                "El margen de ganancia es " + producto.Margen + "\n" +
+                "El valor de IVA es " + producto.IVA + "\n" +
+                "El precio bruto es " + producto.PrecioBruto + "\n"+
+                "El precio de venta es " + producto.PrecioVenta + "\n" +
+                "El proveedor es " + producto.Proveedor + "\n" +
+                "La categoría a la que pertence el producto es " + producto.Categoria + "\n" +
+                "La subcategoría a la que pertenece es " + producto.SubCategoria + "\n" +
+                "\n" +
+                "El nombre del cliente es " + clienteIndividuo.Nombre + "\n" +
+                "El apellido es " + clienteIndividuo.Apellido + "\n" +
+                "El CUIT es " + clienteIndividuo.CUIT + "\n" +
+                "El email es " + clienteIndividuo.Email + "\n" +
+                "El teléfono es " + clienteIndividuo.Telefono + "\n" +
+                "La dirección es " + clienteIndividuo.Direccion);
+
+        }
+    }
+}
